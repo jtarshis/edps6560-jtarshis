093e8fa05dd022789da8eebfb81d124bf99e2b8e
jtarshis <46547961+jtarshis@users.noreply.github.com>
2019-06-04 19:57:29 -0600

    Add files via upload

---
+++ js/variables.js
@@ -0,0 +1,11 @@
+var input_btn_about = document.getElementById("input_btn_about");
+    console.log(input_btn_about);
+
+var input_btn_skills = document.getElementById("input_btn_skills");
+    console.log(input_btn_skills);
+
+var input_btn_work = document.getElementById("input_btn_work");
+    console.log(input_btn_work);
+
+var input_btn_downloads = document.getElementById("input_btn_downloads");
+    console.log(input_btn_downloads);
